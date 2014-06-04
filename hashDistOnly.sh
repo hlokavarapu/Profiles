e0d9ee8973473701c73330c7be837568584bbca3
Vandemar <hlokavarapu@ucdavis.edu>
2014-06-04 00:21:18 -0700

    Temporary fix - updated Readme

---
+++ hashDistOnly.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+git clone https://github.com/hashdist/hashdist.git 
+cd hashdist
+
+#adding to path
+if [ -f ~/.bashrc ];
+then 
+  echo "export PATH=$PWD/bin:\$PATH" >> ~/.bashrc
+  source ~/.bashrc
+else
+  export PATH=$PWD/bin:$PATH
+fi
+
+pathToHit="`which hit`"
+
+if [ -f $pathToHit ];
+then 
+  hit init-home
+fi
+

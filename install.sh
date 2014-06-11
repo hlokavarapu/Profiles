733486b63ce1d8daebb23ebcc735cb44b1a9b06e
vandemar <lokavarapuh@gmail.com>
2014-06-11 16:46:22 -0700

    linear checks to install script

--- install.sh
+++ install.sh
@@ -2,19 +2,17 @@
 #Author: Harsha Lokavarapu
 
 #Downloading utility tool hit
-if [ ! -d hashdist ]; then
-git clone https://github.com/hashdist/hashdist.git 
-fi
+if [ ! -d ~/.hashdist ]; 
+then
+  if [ ! -d hashdist ]; then
+    git clone https://github.com/hashdist/hashdist.git 
+  fi
 
-#adding to path
+  #adding to path
   echo "export PATH=$PATH:$PWD/hashdist/bin" >> ~/.bash_profile
   source ~/.bash_profile
 
-pathToHit="$PWD/hashdist/bin"
-
-if [ ! -d ~/.hashdist ];
-then 
-  $pathToHit/hit init-home
+  $PWD/hashdist/bin/hit init-home
 fi
 
 if [ ! -d hashstack ];
